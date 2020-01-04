3590493bd7a39c26219ab772b21c0ea9746875c8
Tedu <490148497@qq.com>
2020-01-04 09:17:59 +0800

    登录和注册功能 检查登录装饰器

--- index/urls.py
+++ index/urls.py
@@ -5,4 +5,8 @@ from index import views
 
 urlpatterns = [
     url(r'^index', views.index_views),
+    url(r'^index_my$', views.index_my_view),
+    url(r'^my_ip$', views.index_my_ip),
+    url(r'^my_bj$', views.index_my_bj),
+    url(r'^my_mim$', views.index_my_mim),
 ]

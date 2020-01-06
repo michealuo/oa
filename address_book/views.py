125a0ad6aa80a88a62b8f874ba3c21f2cc57a5dd
Tedu <1229009814@qq.com>
2020-01-06 14:21:02 +0800

    通讯录修改

--- address_book/views.py
+++ address_book/views.py
@@ -14,7 +14,7 @@ def address_book_view(request):
 def address_book_list(request):
     if request.method == "GET":
         all_user = User.objects.all()
-        paginator = Paginator(all_user, 12)
+        paginator = Paginator(all_user, 15)
         # 获取当前页码
         c_page = request.GET.get("page", 1)
         # 初始化当前页的page对象
@@ -25,7 +25,7 @@ def address_book_list(request):
         # print(service)
         query = request.POST.get("query")
         users = User.objects.filter(username=query)
-        paginator = Paginator(users, 12)
+        paginator = Paginator(users, 15)
         # 获取当前页码
         c_page = request.GET.get("page", 1)
         # 初始化当前页的page对象

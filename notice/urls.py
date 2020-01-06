5a91e8239105414c87a2a88e817cdbef62a01dc9
Tedu <1229009814@qq.com>
2020-01-06 17:39:29 +0800

    通知公告修改

--- notice/urls.py
+++ notice/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url('^$', views.index_view),
     url('^notice$', views.notice_view),
+    url('^notice_add$', views.notice_add_view),
 ]
--- notice/views.py
+++ notice/views.py
@@ -9,3 +9,7 @@ def index_view(request):
 
 def notice_view(request):
     return render(request, "notice/GongGao.html")
+
+
+def notice_add_view(request):
+    return render(request, "notice/notice_add.html")

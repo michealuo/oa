466c7a16e147fb36eb871aaa5c9bd3e0636990a6
Tedu <1229009814@qq.com>
2020-01-06 16:43:58 +0800

    通知公告修改

--- notice/views.py
+++ notice/views.py
@@ -4,10 +4,7 @@ from user.models import User
 
 # Create your views here.
 def index_view(request):
-    uid = request.session.get("uid")
-    username = request.session.get("username")
-    user = User.objects.get(id=uid, username=username)
-    return render(request, "notice/index_first.html", locals())
+    return render(request, "notice/index_first.html")
 
 
 def notice_view(request):

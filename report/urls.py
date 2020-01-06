6731095ccba75651f7c10f27ec0ee1f6591d5aa2
Zhang <1075516784@qq.com>
2020-01-06 10:54:10 +0800

    一

---
+++ report/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from report import views
+
+urlpatterns = [
+    url(r"^$",views.first_index),
+    url(r"list^$",views.list),
+]
--- report/views.py
+++ report/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def first_index(request):
+    return render(request, "report/index_first.html")
+
+
+def list(request):
+    return render(request, "report/report_list.html")

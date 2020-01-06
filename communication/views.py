73a7705bca70760d2249446f7a294725e901c779
Tedu <1229009814@qq.com>
2020-01-06 09:28:21 +0800

    通讯录

--- communication/views.py
+++ communication/views.py
@@ -5,15 +5,15 @@ from user.models import User
 
 # Create your views here.
 def address_book_view(request):
-    return render(request, "address_book/index_first.html")
+    return render(request, "communication/index_first.html")
 
 
 def address_book_list(request):
     if request.method == "GET":
         users = User.objects.all()
-        return render(request, "address_book/YuanGonglist.html", locals())
+        return render(request, "communication/YuanGonglist.html", locals())
     elif request.method == "POST":
         service = request.POST.get("department")
         query = request.POST.get("query")
         users = User.objects.filter(username=query)
-        return render(request, "address_book/YuanGonglist.html", locals())
+        return render(request, "communication/YuanGonglist.html", locals())

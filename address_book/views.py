75750324e2a0f97d611562dffcac9e9488d94e8c
Tedu <1229009814@qq.com>
2020-01-07 15:02:55 +0800

    通知公告修改

--- address_book/views.py
+++ address_book/views.py
@@ -6,14 +6,16 @@ from index.views import logging_check
 
 
 # Create your views here.
-# @logging_check
+@logging_check
 def address_book_view(request):
     # uid = request.session.get("uid")
     # username = request.session.get("username")
-    # user = User.objects.get(id=uid, username=username)
-    return render(request, "address_book/index_first.html")
+    uid = request.my_uid
+    user = User.objects.get(id=uid)
+    return render(request, "address_book/index_first.html", locals())
 
 
+@logging_check
 def address_book_list(request):
     if request.method == "GET":
         all_user = User.objects.all()
--- notice/urls.py
+++ notice/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.index_view),
-    url('^notice$', views.notice),
-    url('^notice_add$', views.notice_add_view),
-    url('^notice_view$', views.notice_view),
+    url(r'^$', views.index_view),
+    url(r'^notice$', views.notice),
+    url(r'^notice_add$', views.notice_add_view),
+    url(r'^notice_view$', views.notice_view),
 ]

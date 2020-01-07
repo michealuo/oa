2ed1d9e7c441e7c161306f3ae07e83c41859b2c9
Tedu <1229009814@qq.com>
2020-01-07 17:16:16 +0800

    通知公告修改

--- notice/views.py
+++ notice/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from user.models import User
+from index.views import logging_check
 
 
 # Create your views here.
+@logging_check
 def notice_list(request):
     return render(request, "notice/notice_list.html")
 
 
+@logging_check
 def notice_add_view(request):
     return render(request, "notice/notice_add.html")
 
 
+@logging_check
 def notice_view(request):
     return render(request, "notice/notice.html")

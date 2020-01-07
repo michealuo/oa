47a2bf33208d78b1bbb2c507c693a12308efc318
Tedu <1229009814@qq.com>
2020-01-07 08:56:17 +0800

    通知公告修改

--- notice/urls.py
+++ notice/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url('^$', views.index_view),
-    url('^notice$', views.notice_view),
+    url('^notice$', views.notice),
     url('^notice_add$', views.notice_add_view),
+    url('^notice_view$', views.notice_view),
 ]
--- notice/views.py
+++ notice/views.py
@@ -7,9 +7,13 @@ def index_view(request):
     return render(request, "notice/index_first.html")
 
 
-def notice_view(request):
+def notice(request):
     return render(request, "notice/GongGao.html")
 
 
 def notice_add_view(request):
     return render(request, "notice/notice_add.html")
+
+
+def notice_view(request):
+    return render(request, "notice/GongGao1.html")

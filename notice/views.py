c5bec0967d1c181f6fef2df106ea5e0ac08782a8
Tedu <1229009814@qq.com>
2020-01-07 19:20:14 +0800

    通知公告修改

--- notice/views.py
+++ notice/views.py
@@ -1,19 +1,48 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from user.models import User
 from index.views import logging_check
+from notice.models import Notice_list
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
 @logging_check
 def notice_list(request):
-    return render(request, "notice/notice_list.html")
+    # 全部公告列表显示
+    all_notice = Notice_list.objects.all()
+    paginator = Paginator(all_notice, 15)
+    # 获取当前页码
+    c_page = request.GET.get("page", 1)
+    # 初始化当前页的page对象
+    page = paginator.page(c_page)
+    return render(request, "notice/notice_list.html", locals())
 
 
 @logging_check
 def notice_add_view(request):
-    return render(request, "notice/notice_add.html")
+    # 添加公告
+    if request.method == "GET":
+        return render(request, "notice/notice_add.html")
+    elif request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        print(title)
+        try:
+            Notice_list.objects.create(title=title, content=content)
+        except Exception as e:
+            print("---添加失败---")
+            print(e)
+        return HttpResponseRedirect("/notice/list")
 
 
 @logging_check
 def notice_view(request):
-    return render(request, "notice/notice.html")
+    # 当前公告内容显示
+    id = request.GET.get("id")
+    try:
+        notice = Notice_list.objects.get(id=id)
+        print(notice)
+    except Exception as e:
+        print(e)
+    return render(request, "notice/notice.html", locals())

0be7e366fde79d589e0e059d317696ceb586bedf
Tedu <1229009814@qq.com>
2020-01-07 19:49:21 +0800

    通知公告修改

--- notice/views.py
+++ notice/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 @logging_check
 def notice_list(request):
     # 全部公告列表显示
-    all_notice = Notice_list.objects.all()
+    all_notice = Notice_list.objects.all().order_by("-updated_time")
     paginator = Paginator(all_notice, 15)
     # 获取当前页码
     c_page = request.GET.get("page", 1)
@@ -25,6 +25,7 @@ def notice_add_view(request):
     if request.method == "GET":
         return render(request, "notice/notice_add.html")
     elif request.method == "POST":
+        print(111111)
         title = request.POST.get("title")
         content = request.POST.get("content")
         print(title)

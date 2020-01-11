b13111fb0b378557123cf3927ad64233a93aaa96
Tedu <1229009814@qq.com>
2020-01-11 14:12:43 +0800

    通讯录

--- notice/views.py
+++ notice/views.py
@@ -22,12 +22,7 @@ def notice_list(request):
         query = request.POST.get("query")
         all_notice = Notice_list.objects.all().order_by("-created_time")
         if not query:
-            paginator = Paginator(all_notice, 10)
-            # 获取当前页码
-            c_page = request.GET.get("page", 1)
-            # 初始化当前页的page对象
-            page = paginator.page(c_page)
-            return render(request, "notice/notice_list.html", locals())
+            return HttpResponseRedirect("/notice/list")
         query_list = []
         for notice in all_notice:
             if query in notice.title:

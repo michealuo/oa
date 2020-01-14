c057c315963d04e477565709920b5a3a560d19fe
Tedu <490148497@qq.com>
2020-01-14 09:19:13 +0800

    去掉背景设置

--- index/views.py
+++ index/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 
 # Create your views here.
 # 检查登录装饰器
+from common.Tools import getTimeStamp
 from department.models import Position
 from management.models import Management
 from new_project import settings
@@ -95,19 +96,19 @@ def index_my_ip(request):
         page = paginator.page(c_page)
         return render(request, "index/My_IP.html", locals())
 
-@logging_check
-def index_my_bj(request):
-    if request.method == 'GET':
-        return render(request, 'index/My_BJ.html')
-    if request.method == 'POST':
-        # 处理数据
-        file = request.FILES['myfile']
-        filename = os.path.join(settings.MEDIA_ROOT, file.name)
-        with open(filename, 'wb') as f:
-            data = file.file.read()
-            f.write(data)
-
-    return render(request,'index/My_BJ.html',locals())
+# @logging_check
+# def index_my_bj(request):
+#     if request.method == 'GET':
+#         return render(request, 'index/My_BJ.html')
+#     if request.method == 'POST':
+#         # 处理数据
+#         file = request.FILES['myfile']
+#         filename = os.path.join(settings.MEDIA_ROOT, file.name)
+#         with open(filename, 'wb') as f:
+#             data = file.file.read()
+#             f.write(data)
+#
+#     return render(request,'index/My_BJ.html',locals())
 
 @logging_check
 def index_my_mim(request):
